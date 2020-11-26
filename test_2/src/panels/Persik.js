64ce3c64054c1176f976e00a9c949f43d997c0b9
gur019 <gur019@yandex.ru>
2020-11-26 16:41:11 +0300

    43

--- test_2/src/panels/Persik.js
+++ test_2/src/panels/Persik.js
@@ -7,10 +7,15 @@ import PanelHeaderButton from '@vkontakte/vkui/dist/components/PanelHeaderButton
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 
+import FormLayout from '@vkontakte/vkui/dist/components/FormLayout/FormLayout';
+import CellButton from '@vkontakte/vkui/dist/components/CellButton/CellButton';
+
 import persik from '../img/persik.png';
 import './Persik.css';
 
 const osName = platform();
+
+
 
 const Persik = props => (
 	<Panel id={props.id}>
@@ -22,6 +27,9 @@ const Persik = props => (
 			Persik
 		</PanelHeader>
 		<img className="Persik" src={persik} alt="Persik The Cat"/>
+		<FormLayout>
+            <CellButton onClick={this.onClick.bind(this)}>Запустить долгий процесс</CellButton>
+          </FormLayout>
 	</Panel>
 );
 
